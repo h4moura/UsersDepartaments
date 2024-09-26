03e5b1ae4049695553582d0136211feaddac926d
Halyson Moura <138891994+h4moura@users.noreply.github.com>
2024-09-26 14:07:39 -0300

    Add files via upload

--- src/main/java/com/devhalyson/userdept/controllers/UserController.java
+++ src/main/java/com/devhalyson/userdept/controllers/UserController.java
@@ -1,12 +1,9 @@
 package com.devhalyson.userdept.controllers;
 
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.devhalyson.userdept.entities.User;
 import com.devhalyson.userdept.repositories.UserRepository;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,23 +20,21 @@ public class UserController {
 
     @GetMapping
     public List<User> findAll() {
-        repository.findAll();
-        {
-            List<User> result = repository.findAll();
-            return result;
-        }
+       List<User> result = repository.findAll();
+       return result;
     }
 
     @GetMapping(value = "/{id}")
-    public Optional<User> findById(@PathVariable Long id) {
-        Optional<User> result = repository.findById(id);
-        return result;
+    public User findById(@PathVariable Long id) {
+       User result = repository.findById(id).get();
+       return result;
     }
 
-    @PostMapping(value = "/{id}")
-    public User insrt(@RequestBody User user) {
-        User result = repository.save(user);
-        return result;
+    @PostMapping
+    public User insert(@RequestBody User user) {
+       User result = repository.save(user);
+       return result;
     }
+
 
 }
--- src/main/java/com/devhalyson/userdept/repositories/UserRepository.java
+++ src/main/java/com/devhalyson/userdept/repositories/UserRepository.java
@@ -6,4 +6,6 @@ import com.devhalyson.userdept.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+   
+
 }
