0d5d6506e9e26b54c0197ac47871bb0e46cdee8b
Halyson Moura <138891994+h4moura@users.noreply.github.com>
2024-09-26 21:30:32 -0300

    Add files via upload

--- src/main/java/com/devhalyson/userdept/controllers/UserController.java
+++ src/main/java/com/devhalyson/userdept/controllers/UserController.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.devhalyson.userdept.entities.User;
 import com.devhalyson.userdept.repositories.UserRepository;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
@@ -36,5 +38,23 @@ public class UserController {
        return result;
     }
 
+    @PutMapping(value = "/{id}")
+    public User update(@PathVariable Long id, @RequestBody User user) {
+        // Busca o usuário pelo ID
+        User existingUser = repository.findById(id).orElse(null);
+        
+        if (existingUser != null) {
+            
+            existingUser.setName(user.getName());  
+            existingUser.setEmail(user.getEmail());  
+            User updatedUser = repository.save(existingUser);
+            return updatedUser;
+        } 
+        return null;
+    }
 
+    @DeleteMapping(value = "/{id}")
+    public void delete(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
 }
--- target/classes/import.sql
+++ target/classes/import.sql
@@ -1,5 +1,6 @@
-INSERT INTO tb_department(name) VALUES ('Gestão');
-INSERT INTO tb_department(name) VALUES ('Informática');
+INSERT INTO tb_department(name) VALUES ('Admin'); //Cadastra usr e Remover usr
+INSERT INTO tb_department(name) VALUES ('Membro'); //Adiciona, Remover e Editar Veiculo.
+
 
 INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Maria', 'maria@gmail.com');
 INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Bob', 'bob@gmail.com');
